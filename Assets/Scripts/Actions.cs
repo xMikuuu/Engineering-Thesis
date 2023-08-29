2c28430ca6519006901f4168bd12fd4ecfd26d27
xMikuuu <m.polatynski9@gmail.com>
2023-08-29 21:46:14 +0200

    Added arena with basic player and opponent + 2 action buttons

---
+++ Assets/Scripts/Actions.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+using UnityEngine.UI;
+
+
+public class Actions : MonoBehaviour
+{
+    public void MoveLeft(){
+        Debug.Log("Moving Left");
+    }
+}
