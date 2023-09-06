528d7a7412d51b463b20ce90057d5243f1f97dc6
xMikuuu <m.polatynski9@gmail.com>
2023-09-06 19:10:19 +0200

    HealthBar update (not working... YET)

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,20 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
-using System;
-using UnityEngine.UI;
 
-public class Player : MonoBehaviour
+public class Health : MonoBehaviour
 {
-
-
-    
-    [SerializeField] public TMP_Text hitpoints;
-
-
-
-
     // Start is called before the first frame update
     void Start()
     {
